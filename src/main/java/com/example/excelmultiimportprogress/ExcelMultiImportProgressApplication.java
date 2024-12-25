f07020d572b2ec942fec45b5ff14578814f3830f
pyf <peng.yifu@grandlynn.com>
2024-12-25 17:29:25 +0800

    init

---
+++ src/main/java/com/example/excelmultiimportprogress/ExcelMultiImportProgressApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelmultiimportprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelMultiImportProgressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelMultiImportProgressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/excelmultiimportprogress/ExcelMultiImportProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excelmultiimportprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelMultiImportProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
